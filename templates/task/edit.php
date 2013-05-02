5cc3d2500f6d519bf6a37cda3ba08cb7d03f6175
Dwi Wijonarko <d.wijonarko@gmail.com>
2013-05-02 19:31:05 +0700

    modofikasi tampilan edit.php dengan twig template dan bootstrap css

--- templates/task/edit.php
+++ templates/task/edit.php
@@ -1,10 +1,30 @@
-<h2>Edit</h2>
-<form action="/<?php echo $task->id ?>/edit" method="POST">
-	<label for="name">Name</label><input type="text" name="name" value="<?php echo $task->name ?>"><br>
-	<?php if ($task->done===1): ?>
-		<input type="checkbox" name="done" value=1 checked="true">Done<br>	
-	<?php else: ?>
-		<input type="checkbox" name="done" value=1 >Done<br>	
-	<?php endif ?>
-	<input type="submit" name="submit" value="save">
-</form>
+{% extends "layout.html" %} 
+{% block content %}
+    <form class="form-horizontal" action="/{{task.id}}/edit" method="POST">
+    	<fieldset>
+		<legend>Edit</legend>
+	    <div class="control-group">
+		    <label class="control-label" for="taskName">Task Name</label>
+	    	<div class="controls">
+			    <input type="text" id="taskName" placeholder="Task Name" name="name" value="{{task.name}}">
+	    	</div>
+	    </div>
+	    <div class="control-group">
+		    <label class="control-label" for="taskStatus">Status</label>
+		    <div class="controls">
+			    {% if (task.done==1) %}
+					<input type="checkbox" name="done" value=1 checked="true">Done<br>	
+				{% else %}
+					<input type="checkbox" name="done" value=1 > Done<br>	
+				{% endif %}
+				
+		    </div>
+	    </div>
+	    <div class="control-group">
+		    <div class="controls">
+			    <input type="submit" name="submit" value="save" class="btn btn-primary">
+		    </div>
+	    </div>
+	</fieldset>
+    </form>
+{% endblock %}
